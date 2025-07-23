bd1792a00bc5263a91c4a9da565e9684ccc2bd69
yashbim <ybimsara03@gmail.com>
2025-07-24 00:04:47 +0530

    Initialized from Spring

---
+++ src/main/kotlin/com/example/e_sugarcubes/Session.kt
@@ -0,0 +1,12 @@
+package com.example.e_sugarcubes
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Table;
+import lombok.Data
+//
+//@Data
+//@Entity
+//@Table(name = "sessions")
+
+class Session {
+}
