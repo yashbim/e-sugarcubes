4a831bbbc47c5c873a7850064eb9caea99b2baa1
yashbim <ybimsara03@gmail.com>
2025-07-24 00:17:43 +0530

    Created session option menu

--- src/main/kotlin/com/example/e_sugarcubes/ESugarcubesApplication.kt
+++ src/main/kotlin/com/example/e_sugarcubes/ESugarcubesApplication.kt
@@ -2,14 +2,37 @@ package com.example.e_sugarcubes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class ESugarcubesApplication
 
 fun main(args: Array<String>) {
 	runApplication<ESugarcubesApplication>(*args)
-	print("Hello AIESECer!");
+	val scanner = Scanner(System.`in`)
+	println("Hello AIESECer!");
+	var choice = 0;
+	print("Enter your option: \n 1. Create Session \n 2. Join Session");
+	choice = scanner.nextInt();
+
+	if (choice == 1) {
+		createSession();
+	} else if (choice == 2) {
+		joinSession();
+	} else {
+		println("Error!");
+	}
+
 
 }
 
 
+fun createSession(){
+	print("Let's create a session")
+}
+
+fun joinSession(){
+	print("Let's join a session")
+}
+
+fun postCube(){}
