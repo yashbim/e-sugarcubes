bb37cacd3788d1938c757e0ff5fb3dde3f7454a4
yashbim <ybimsara03@gmail.com>
2025-07-29 01:24:35 +0530

    Reset project

---
+++ src/main/kotlin/com/example/e_sugarcubes/ESugarcubesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.e_sugarcubes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ESugarcubesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ESugarcubesApplication>(*args)
+}
